c8e560854943d1be84976f758624b99d7c4502e2
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2021-02-05 23:31:15 +0400

    Added Command base

---
+++ Leak.Domain.Core/Command/Command.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediatR;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Leak.Domain.Core.Command
+{
+    public abstract class Command : IRequest<ValidationResult>
+    {
+        public ValidationResult ValidationResult { get; set; }
+
+        protected Command()
+        {
+            ValidationResult = new ValidationResult();
+        }
+
+        public virtual bool IsValid()
+        {
+            return ValidationResult.IsValid;
+        }
+    }
+}
---
+++ Leak.Domain.Core/Command/CommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+
+namespace Leak.Domain.Core.Command
+{
+    public abstract class CommandValidator : AbstractValidator<Command>
+    {
+    }
+}
