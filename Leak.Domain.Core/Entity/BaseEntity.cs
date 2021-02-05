2b31f980f8a7b1e51baa84865ecb9d29a24c331b
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2021-02-05 22:29:43 +0400

    Add project files.

---
+++ Leak.Domain.Core/Entity/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leak.Domain.Core.Entity
+{
+    public abstract class BaseEntity<TKey> : IEntity
+    {
+        public TKey Id { get; set; }
+    }
+}
---
+++ Leak.Domain.Core/Entity/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leak.Domain.Core.Entity
+{
+    public interface IEntity
+    {
+    }
+}
