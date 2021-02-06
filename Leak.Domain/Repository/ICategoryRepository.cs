3cfe40551c31c2d7547fae634c0e204fb876be69
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2021-02-06 13:17:43 +0400

    Added category repo interface

---
+++ Leak.Domain/Repository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Leak.Domain.Core.Repository;
+using Leak.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leak.Domain.Repository
+{
+    public interface ICategoryRepository : IRepository<Category, int>
+    {
+        void Update(Category category);
+    }
+}
