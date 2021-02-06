69888ec4ad9bc3c30d8f1da2edfc3fafc3f40d7e
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2021-02-06 21:50:56 +0400

    Added mapping profiles

---
+++ Leak.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Leak.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leak.Application.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+        }
+    }
+}
---
+++ Leak.Application/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leak.Application.AutoMapper
+{
+    class ViewModelToDomainMappingProfile
+    {
+    }
+}
