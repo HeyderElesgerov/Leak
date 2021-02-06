ef62b3b057b05beeefb156e7c607f6c86bb71bf1
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2021-02-06 13:37:34 +0400

    Implemented CategoryCommandHandler

--- Leak.Domain/Commands/Category/CategoryCommandHandler.cs
+++ Leak.Domain/Commands/Category/CategoryCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation.Results;
 using Leak.Domain.Core.Command;
+using Leak.Domain.Repository;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -14,19 +15,64 @@ namespace Leak.Domain.Commands.Category
         IRequestHandler<DeleteCategoryCommand, ValidationResult>,
         IRequestHandler<UpdateCategoryCommand, ValidationResult>
     {
+        ICategoryRepository _categoryRepository;
+
+        public CategoryCommandHandler(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
         public Task<ValidationResult> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            if (!request.IsValid())
+                return Task.FromResult(request.ValidationResult);
+
+            _categoryRepository.Add(new Models.Category(request.CategoryTitle));
+            _categoryRepository.Commit();
+
+            return Task.FromResult(ValidationResult);
         }
 
         public Task<ValidationResult> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            if (!request.IsValid())
+                return Task.FromResult(request.ValidationResult);
+
+            var category = _categoryRepository.Find(request.Id);
+
+            if(category == null)
+            {
+                AddError("Category not found");
+            }
+            else
+            {
+                _categoryRepository.Delete(category);
+                _categoryRepository.Commit();
+            }
+
+            return Task.FromResult(ValidationResult);
         }
 
         public Task<ValidationResult> Handle(UpdateCategoryCommand request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            if (!request.IsValid())
+                return Task.FromResult(request.ValidationResult);
+
+            var category = _categoryRepository.Find(request.Id);
+
+            if(category == null)
+            {
+                AddError("Category not found");
+            }
+            else
+            {
+                category.ChangeTitle(request.CategoryTitle);
+
+                _categoryRepository.Update(category);
+                _categoryRepository.Commit();
+            }
+
+            return Task.FromResult(ValidationResult);
         }
     }
 }
