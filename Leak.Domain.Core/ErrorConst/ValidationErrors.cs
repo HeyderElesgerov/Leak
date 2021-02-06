778688349ebe7817910ae48328f1169e1553cad2
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2021-02-06 12:10:41 +0400

    Added Error constants

---
+++ Leak.Domain.Core/ErrorConst/ValidationErrors.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leak.Domain.Core.ErrorConst
+{
+    public static class ValidationErrors
+    {
+        public static string CannotBeNull = "Field cannot be null";
+
+    }
+}
