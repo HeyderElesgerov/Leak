15b4bb69be51e3043f2807ce0b2926e4e4c99009
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2021-02-07 16:56:38 +0400

    Added category service

---
+++ Leak.Application/Interfaces/ICategoryService.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation.Results;
+using Leak.Application.ViewModels.Category;
+using Leak.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leak.Application.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryViewModel>> GetAllCategories();
+
+        Task<ValidationResult> Add(CategoryViewModel categoryViewModel);
+
+        Task<ValidationResult> Delete(CategoryViewModel categoryViewModel);
+
+        Task<ValidationResult> Update(CategoryViewModel categoryViewModel);
+    }
+}
---
+++ Leak.Application/Services/CategoryService.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using FluentValidation.Results;
+using Leak.Application.Interfaces;
+using Leak.Application.ViewModels.Category;
+using Leak.Domain.Commands.Category;
+using Leak.Domain.Models;
+using Leak.Domain.Repository;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leak.Application.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private IMapper _mapper;
+        private IMediator _mediator;
+        private ICategoryRepository _categoryRepository;
+
+        public CategoryService(
+            IMapper mapper, 
+            IMediator mediator, 
+            ICategoryRepository categoryRepository)
+        {
+            _mapper = mapper;
+            _mediator = mediator;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<ValidationResult> Add(CategoryViewModel categoryViewModel)
+        {
+            CreateCategoryCommand createCategoryCommand = 
+                _mapper.Map<CreateCategoryCommand>(categoryViewModel);
+
+            return await _mediator.Send(createCategoryCommand);
+        }
+
+        public async Task<ValidationResult> Delete(CategoryViewModel categoryViewModel)
+        {
+            DeleteCategoryCommand deleteCategoryCommand = _mapper.Map<DeleteCategoryCommand>(categoryViewModel);
+
+            return await _mediator.Send(deleteCategoryCommand);
+        }
+
+        public async Task<IEnumerable<CategoryViewModel>> GetAllCategories()
+        {
+            return _mapper.Map<IEnumerable<CategoryViewModel>>(
+                await _categoryRepository.GetAll());
+        }
+
+        public async Task<ValidationResult> Update(CategoryViewModel categoryViewModel)
+        {
+            UpdateCategoryCommand updateCategoryCommand = _mapper.Map<UpdateCategoryCommand>(categoryViewModel);
+
+            return await _mediator.Send(updateCategoryCommand);
+        }
+    }
+}
