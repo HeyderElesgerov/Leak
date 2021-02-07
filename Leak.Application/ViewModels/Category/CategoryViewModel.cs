18c1939e09f9b4c92a5608aa6af1b47408c71560
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2021-02-07 16:32:40 +0400

    Added category viewmodel

---
+++ Leak.Application/ViewModels/Category/CategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leak.Application.ViewModels.Category
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
