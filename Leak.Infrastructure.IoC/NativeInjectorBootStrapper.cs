0837589b23f463463b66d34cc3311c2233b36e55
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2021-02-07 22:10:23 +0400

    Added initial ioc

--- Leak.Infrastructure.IoC/NativeInjectorBootStrapper.cs
+++ Leak.Infrastructure.IoC/NativeInjectorBootStrapper.cs
@@ -1,10 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FluentValidation.Results;
+using Leak.Application.Interfaces;
+using Leak.Application.Services;
+using Leak.Domain.Commands.Category;
+using Leak.Infrastructure.Data.Context;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Leak.Infrastructure.IoC
 {
-    class NativeInjectorBootStrapper
+    public static class NativeInjectorBootStrapper
     {
+        public static void RegisterServices(IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.AddDbContext<LeakDbContext>(options =>
+            {
+                string connectionString = configuration.GetConnectionString("DefaultConnection");
+                options.UseSqlServer(connectionString);
+            });
+
+            // Domain - Commands
+            serviceCollection.AddScoped<IRequestHandler<CreateCategoryCommand, ValidationResult>, CategoryCommandHandler>();
+            serviceCollection.AddScoped<IRequestHandler<UpdateCategoryCommand, ValidationResult>, CategoryCommandHandler>();
+            serviceCollection.AddScoped<IRequestHandler<DeleteCategoryCommand, ValidationResult>, CategoryCommandHandler>();
+
+            //Repos
+            //serviceCollection.AddScoped<ICategoryRepository>();
+
+            //Services
+            serviceCollection.AddScoped<ICategoryService, CategoryService>();
+        }
     }
 }
