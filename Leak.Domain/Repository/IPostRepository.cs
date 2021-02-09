73aab99961193ebcdf7ef2716aabaa0eabc32643
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2021-02-09 11:45:25 +0400

    Added UoW

---
+++ Leak.Domain/Repository/IPostRepository.cs
@@ -0,0 +1,15 @@
+﻿using Leak.Domain.Core.Repository;
+using Leak.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leak.Domain.Repository
+{
+    public interface IPostRepository : IRepository<Post, int>
+    {
+        Task Update(Post post);
+    }
+}
---
+++ Leak.Domain/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Leak.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leak.Domain.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IPostRepository PostRepository { get; set; }
+        IBlogRepository BlogRepository { get; set; }
+        ICategoryRepository CategoryRepository { get; set; }
+        IUrlRepository UrlRepository { get; set; }
+
+        Task Commit();
+    }
+}
