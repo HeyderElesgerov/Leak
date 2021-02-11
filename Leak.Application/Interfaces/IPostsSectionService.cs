7abbaf100b24686ce98b5c6317748ac72876d5c9
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2021-02-11 21:57:27 +0400

    Implemented the section service

---
+++ Leak.Application/Interfaces/IPostsSectionService.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation.Results;
+using Leak.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Leak.Application.Interfaces
+{
+    public interface IPostsSectionService<TSection>
+        where TSection : SpecialPostsSection
+    {
+        Task<ValidationResult> AddPostToSection(int postId);
+        Task<ValidationResult> RemovePostFromSection(int postId);
+    }
+}
---
+++ Leak.Application/Services/PostsSectionService.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation.Results;
+using Leak.Application.Interfaces;
+using Leak.Domain.Commands.SpecialPostsSection;
+using Leak.Domain.Models;
+using MediatR;
+using System.Threading.Tasks;
+
+namespace Leak.Application.Services
+{
+    public class PostsSectionService<TSection> : IPostsSectionService<TSection>
+        where TSection : SpecialPostsSection
+    {
+        private IMediator _mediator;
+
+        public PostsSectionService(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<ValidationResult> AddPostToSection(int postId)
+        {
+            return await _mediator.Send(new AddPostToPostsSectionCommand<TSection>(postId));
+        }
+
+        public async Task<ValidationResult> RemovePostFromSection(int postId)
+        {
+            return await _mediator.Send(new RemovePostFromPostsSectionCommand<TSection>(postId));
+        }
+    }
+}
