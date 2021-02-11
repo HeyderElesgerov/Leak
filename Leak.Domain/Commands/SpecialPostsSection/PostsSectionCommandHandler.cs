3d05a2fb36cc2037cbdbaf38345e68f76ff9d53a
Heyder.Elesgerov <50196884+HeyderElesgerov@users.noreply.github.com>
2021-02-11 21:47:02 +0400

    Implemented section handlers

--- Leak.Domain/Commands/SpecialPostsSection/PostsSectionCommandHandler.cs
+++ Leak.Domain/Commands/SpecialPostsSection/PostsSectionCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation.Results;
 using Leak.Domain.Core.Command;
+using Leak.Domain.Repository;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -13,16 +14,63 @@ namespace Leak.Domain.Commands.SpecialPostsSection
     public class PostsSectionCommandHandler<TSection> : CommandHandler,
         IRequestHandler<AddPostToPostsSectionCommand<TSection>, ValidationResult>,
         IRequestHandler<RemovePostFromPostsSectionCommand<TSection>, ValidationResult>
-        where TSection : Models.SpecialPostsSection
+        where TSection : Models.SpecialPostsSection, new()
     {
-        public Task<ValidationResult> Handle(AddPostToPostsSectionCommand<TSection> request, CancellationToken cancellationToken)
+        private readonly ISpecialPostsSectionRepository<TSection> _sectionRepository;
+
+        //we jus use to fetch, not save
+        private readonly IPostRepository _postRepository;
+
+        public PostsSectionCommandHandler(ISpecialPostsSectionRepository<TSection> repo, IPostRepository postRepository)
         {
-            throw new NotImplementedException();
+            _sectionRepository = repo;
+            _postRepository = postRepository;
         }
 
-        public Task<ValidationResult> Handle(RemovePostFromPostsSectionCommand<TSection> request, CancellationToken cancellationToken)
+        public async Task<ValidationResult> Handle(AddPostToPostsSectionCommand<TSection> request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            int postId = request.PostId;
+            bool postExists = await _postRepository.Any(p => p.Id == postId);
+
+            if (!postExists)
+            {
+                AddError("Post not found");
+            }
+            else
+            {
+                bool postExistsInThisSection = await _sectionRepository.Any(s => s.PostId == postId);
+
+                if (postExistsInThisSection)
+                {
+                    AddError("This post already in this section");
+                }
+                else
+                {
+                    await _sectionRepository.Add(new TSection() { PostId = postId });
+                    await _sectionRepository.Commit();
+                }
+            }
+
+            return ValidationResult;
+        }
+
+        public async Task<ValidationResult> Handle(RemovePostFromPostsSectionCommand<TSection> request, CancellationToken cancellationToken)
+        {
+            int postId = request.PostId;
+
+            TSection section = (await _sectionRepository.GetWhere(s => s.Id == postId)).FirstOrDefault();
+
+            if (section != null)
+            {
+                AddError("Post already not in this section");
+            }
+            else
+            {
+                await _sectionRepository.Delete(section);
+                await _sectionRepository.Commit();
+            }
+
+            return ValidationResult;
         }
     }
 }
